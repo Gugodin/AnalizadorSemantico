9718f905367446feabbf4f659655073673a142b9
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2022-03-29 18:31:09 -0600

    final commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import string
 import re
-import subprocess
+import subprocess, shlex
 import sys
 from tabnanny import check
 from PySide6.QtCore import *
@@ -104,7 +104,7 @@ def comando(entry):
             apuntador += 1
             responseP = isPalabra(entry[apuntador])
 
-            
+            print(f'responseP {responseP}')
 
             if responseD is None and responseP == 'Correcto':
                 return 'Correcto'
@@ -246,7 +246,7 @@ def direccion(entry):
         return 'Error'
 
 def isPalabra(palabra):
-    p = re.compile('(\w|_|.|-|\s)+')
+    p = re.compile('([a-z]|[A-B]|_|.|-)+')
 
     if p.fullmatch(palabra) is None: 
         return 'Error'
@@ -258,102 +258,203 @@ class main(QMainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.tokens= [
+        ['crear','mkdir'],
+        ['mover','cd'],
+        ['mostrar','ls'],
+        ['editar','mv'],
+        ['remover','rm'],
+        ['crearA','touch'],
+        ['diagonal','/'],
+        ['guion','-'],
+        ['guionB','_'],
+        ['punto','.'],
+        ['numero',list(string.digits)],
+        ['letra',list(string.ascii_letters)]
+        ]
         self.ui.pushButton.clicked.connect(self.comprobacion)
+
+    def getTockens(self):
+        
+        command = self.ui.lineEdit.text().strip(' ').split()
+        print(f'Comando: {command}')
+        listTokens = []
+        error = ''
+        for i in range(len(command)):
+            band = False
+            palabra = command[i]
+
+            # print(f'Bandera antes del if {band}')
+            for e in range(len(self.tokens)):
+                if palabra == self.tokens[e][1]:
+                    print(f'Token: {self.tokens[e][0]}')
+                    listTokens.append(self.tokens[e][0])
+                    band = True
+                    break
+               
+
+            if band == False:       
+                for z in range(len(palabra)):
+                    charFinded = False
+
+                    char = palabra[z]
+
+                    # print(char)
+                   
+                    for e in range(len(self.tokens)):
+
+                        # print(self.tokens[e])
+
+                        if self.tokens[e][0] == 'numero' or self.tokens[e][0] == 'letra':
+
+                            if self.tokens[e][1].count(char) > 0:
+                                
+                                print(f'Token: {self.tokens[e][0]}')
+                                listTokens.append(self.tokens[e][0])
+                                band = True
+                                charFinded = True
+                                break
+                        
+                        elif char == self.tokens[e][1]:
+                            print(f'Token: {self.tokens[e][0]}')
+                            listTokens.append(self.tokens[e][0])
+                            band = True
+                            charFinded = True
+                            break
+
+                    if charFinded == False:
+                        band = False
+                        return f'Cadena invalida\nEl carater siguiente no existe dentro de nuestros tokens {char}'
+                                                
+            if band == False:
+                listTokens.clear()
+                if error == '':
+                    listTokens.append(f'Cadena invalida\nEl carater siguiente no existe dentro de nuestros tokens {command[i]}')
+                else:
+                    listTokens.append(error)
+                break
+        cadenaT = ''
+
+        for i in range(len(listTokens)):
+            cadenaT = cadenaT + listTokens[i] + ' '
     
     def comprobacion(self):
         global apuntador
         global apuntadorC
-        entry = self.ui.lineEdit.text().strip(' ').split()
-        response = comando(entry = entry)
 
-        self.ui.message.setText(response+' sintacticamente')
+        errorTo = self.getTockens()
 
-        if response == 'Correcto':
-          
-
-            # PRIMERO VERIFICAMOS QUE LAS RUTAS SEAN EXISTENTES EN EL COMANDO EXISTAN
-            error = verifyRoutesAndArchives(entry)
-
-            print(error) 
-            # error = True
-
-            # rm /Users/humbe/Downloads/ message.txt
-
-            for i in range(len(entry)):
-                palabra = entry[i]
-                
-                if isDirection(palabra):
-                    l = list(palabra)
-                    l.pop(len(l)-1)
-                    l.append('"')
-                    l.insert(0,'"')
-                    # print(l)
-
-                    palabra = ''.join(l)
-                    palabra = palabra.replace('/','\\')
-                    # print(f'Palabra: {palabra}')
-
-                    entry[i]= palabra
-
-
-            ########################
-            # if entry[0] =='mkdir' and error=='Error de Archivo':
-            #     error=False                   
-            # entry = ' '.join(entry)
-            # if entry =='ls -l':
-            #     error=False
-
-            if error == False:
-                pipe = subprocess.Popen(entry,  stdout=subprocess.PIPE).stdout
-            
-                output = pipe.read()
-
-                texto = list(output.__str__())
-                # texto[0] = ''
-                texto[0] = ''
-                texto=''.join(texto)
-                texto = texto.replace('\'','')
-                # print(texto)
-                texto = texto.split('\\n')
-                texto = '\n'.join(texto)
-                # print(texto)
-                # texto = texto.replace('\'','')
-                
-                self.ui.consola.setText(texto)
-
-
-
-            # print(entry)
-                
-            # r'ls "C:\Users\humbe\Documents\Visual Studio Code\Python\Dianita"'
-            # print(v)
-            elif error=='Error de ruta':
-                self.ui.consola.setText('Ruta no existente')
-            
-            elif error=='Error de Archivo':
-                self.ui.consola.setText('Archivo no existente')
-            
-            
-
-
-
-            
-            
+        print(errorTo)
         
+        if errorTo == None:
+            entry = self.ui.lineEdit.text().strip(' ').split()
+            response = comando(entry = entry)
+            
+            self.ui.message.setText(response+' sintacticamente')
 
-        apuntador = 0
-        apuntadorC = 0
+            if response == 'Correcto':
+            
+
+                # PRIMERO VERIFICAMOS QUE LAS RUTAS SEAN EXISTENTES EN EL COMANDO EXISTAN
+                error = verifyRoutesAndArchives(entry)
+
+                print(error) 
+                
+                prueba = []
+                for i in range(len(entry)):
+                    palabra = entry[i]
+                    
+                    if isDirection(palabra):
+                        l = list(palabra)
+                        if entry[0] =='ls':
+                            l.pop(len(l)-1)
+                        l.append('"')
+                        l.insert(0,'C:')
+                        l.insert(0,'"')
+                        if entry[0] != 'ls':
+                            l.insert(len(l)-1,entry[i+1])
+                        # print(l)
+                        palabra = ''.join(l)
+                        palabra = palabra.replace('/','\\')
+                        print(f'Palabra: {palabra}')
+                        prueba.append(palabra)
+
+                        entry[i]= palabra
+                        
+                        # entry.pop()
+                        # entry.insert(len(entry), palabra)
+                        # print(prueba)
+
+
+                
+                if error == False:
+
+                    # subprocess.call(shlex.split(' mv "C:\\Users\\humbe\\Desktop\\prueba\\c.txt" "C:\\Users\\humbe\\Desktop\\prueba\\a.txt"'))    
+                    if entry[0] != 'ls':
+                        prueba.insert(0,entry[0])
+                        entry = prueba
+                        
+                    print(entry)
+                    if entry[0] == 'ls' or entry[0] == 'mkdir':
+                        entry = ' '.join(entry)
+                        print(entry)
+                        pipe = subprocess.Popen(entry,  stdout=subprocess.PIPE).stdout
+                    
+                        output = pipe.read()
+
+                        texto = list(output.__str__())
+                        # texto[0] = ''
+                        texto[0] = ''
+                        texto=''.join(texto)
+                        texto = texto.replace('\'','')
+                        # print(texto)
+                        texto = texto.split('\\n')
+                        texto = '\n'.join(texto)
+                        # print(texto)
+                        # texto = texto.replace('\'','')
+                        
+                        self.ui.consola.setText(texto)
+                    elif entry[0]=='rm':
+                        entry = ' '.join(entry)
+                        subprocess.call(shlex.split(entry))
+                        self.ui.consola.setText('Archivo eliminado')
+                    elif entry[0]=='mv':
+                        entry = ' '.join(entry)
+                        print(f'mv entry {entry}')
+                        subprocess.call(shlex.split(entry))
+                        self.ui.consola.setText('Archivo editado')
+                    elif entry[0]=='touch':
+                        entry = ' '.join(entry)
+                        subprocess.call(shlex.split(entry))
+                        self.ui.consola.setText('Archivo creado')
+
+
+
+
+            
+                elif error=='Error de ruta':
+                    self.ui.consola.setText('Ruta no existente')
+                
+                elif error=='Error de archivo':
+                    self.ui.consola.setText('Archivo no existente')
+                
+            
+            # self.ui.consola.setText('')
+            apuntador = 0
+            apuntadorC = 0
+        else:
+            self.ui.consola.setText(errorTo)
         
 def verifyRoutesAndArchives(entry):
 
     
     #EN ESTE METODO VERIFICAMOS PRIMERO QUE COMANDO ES YA QUE CADA EN UNOS CASOS NO SE DEBE DE VERIFICAR SI EL ARCHIVO EXISTE
-    print(entry)
+    
 
     rutas = []
     archivos = []
 
-    if entry[0] == 'mkdir':
+    if entry[0] == 'mkdir' or entry[0] == 'touch':
         return False
     elif entry[0] == 'cd':
         #EL CD SOLAMENTE SE VERIFICARA SI TRAE UN ARCHIVO O RUTA SI NO LO REGRESAMOS AUTOMATICAMENTE
@@ -403,33 +504,13 @@ def verifyRoutesAndArchives(entry):
          if os.path.exists(rutas[i]) == False:
              return 'Error de ruta'
 
-    for i in range(len(archivos)):
-         if os.path.exists(archivos[i]) == False:
-             return 'Error de archivo'
+    # for i in range(len(archivos)):
+    
+    if len(archivos) >0 :
+        if os.path.exists(rutas[0]+archivos[0]) == False:
+            return 'Error de archivo'
 
     return False
-
-
-
-    # for i in range(len(entry)):
-    #             if isDirection(entry[i]):
-    #                 if os.path.exists(entry[i]) == False:
-    #                     error= 'Error de ruta'
-    #                     break
-    #                 elif i+1 < len(entry):
-    #                     # print('primer error archivo')   
-    #                     if isPalabra(entry[i+1]):
-    #                         if os.path.exists(entry[i]) == False:
-    #                             error= 'Error de Archivo'
-    #                             break
-    #             if isDirection(entry[len(entry)-1]) == False:
-    #                 print(entry[len(entry) - 1])
-    #                 if isPalabra(entry[len(entry)-1]):
-    #                     print('se error archivo')   
-    #                     if isPalabra(entry[i+1]):
-    #                         if os.path.exists(entry[i]) == False:
-    #                             error= 'Error de Archivo'
-    #                             break
 
 def isDirection (sDirec):
     if sDirec[len(sDirec)-1] == '/' and sDirec[0] == '/':
@@ -438,17 +519,8 @@ def isDirection (sDirec):
         return False
 
 if __name__ == '__main__':
-
+    # subprocess.call(shlex.split('cd ..'))
     app = QApplication(sys.argv)
     window = main()
     window.show()
     sys.exit(app.exec())
-
-    # print(direccion('a'))
-    # entry = input('Escribe el comando de linux: ').strip(' ').split()
-
-    # comando(entry=entry)
-    # print('Termino bien')
-
-    # print(string.ascii_letters.__contains__('a'))
-
